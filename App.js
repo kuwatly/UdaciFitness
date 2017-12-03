ff1e18d7a76a2f0436cf76709e4f5643937c8167
Iyad Kuwatly <ikuwatly@gmail.com>
2017-12-02 22:59:23 -0500

    Add pizza icon

--- App.js
+++ App.js
@@ -1,19 +1,14 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Ionicons } from "@expo/vector-icons"
 
 export default class App extends React.Component {
-  componentDidMount() {
-    console.log('Before');
-    debugger;
-    console.log('After');
-  }
 
   render() {
     return (
       <View style={styles.container}>
         <Text>Hello World 2</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+        <Ionicons name="ios-pizza" color="red" size = {100}/>
       </View>
     );
   }

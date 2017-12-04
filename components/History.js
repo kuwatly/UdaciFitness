73636def23ff4193947133a812fada0d3e014880
Iyad Kuwatly <ikuwatly@gmail.com>
2017-12-03 20:21:17 -0500

    Add MetricCard component

--- components/History.js
+++ components/History.js
@@ -1,12 +1,13 @@
-import React, { Component } from 'react'
-import { View, Text, StyleSheet, Platform, TouchableOpacity } from 'react-native'
-import { connect } from 'react-redux'
-import { receiveEntries, addEntry } from '../actions'
-import { timeToString, getDailyReminderValue } from '../utils/helpers'
-import { fetchCalendarResults } from '../utils/api'
-import UdaciFitnessCalendar from 'udacifitness-calendar'
-import { white } from '../utils/colors'
-import DateHeader from './DateHeader'
+import React, { Component } from 'react';
+import { View, Text, StyleSheet, Platform, TouchableOpacity } from 'react-native';
+import { connect } from 'react-redux';
+import { receiveEntries, addEntry } from '../actions';
+import { timeToString, getDailyReminderValue } from '../utils/helpers';
+import { fetchCalendarResults } from '../utils/api';
+import UdaciFitnessCalendar from 'udacifitness-calendar';
+import { white } from '../utils/colors';
+import DateHeader from './DateHeader';
+import MetricCard from './MetricCard';
 
 class History extends Component {
   componentDidMount () {
@@ -35,7 +36,7 @@ class History extends Component {
         : <TouchableOpacity
           onPress={() => console.log('Pressed!')}
         >
-          <Text>{JSON.stringify(metrics)}</Text>
+          <MetricCard date={formattedDate} metrics={metrics} />
         </TouchableOpacity>}
     </View>
   )
